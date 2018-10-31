599934f2ef62994c40bfb070480742e7f03deaa2
elangovan <elangovan2me@gmail.com>
2018-10-31 19:31:10 +0530

    Modified layout

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,8 +4,8 @@ import React from 'react';
 class Main extends React.Component {
   render() {
     return (
-        <div className="row">
-            <div className="row container">
+        <div className="row  col-sm-12 col-md-12 col-lg-12 container-fluid">
+            <div className="row container container-fluid">
                 <div className="jumbotron container jumbotron-bg">
                     <div className="container">
                         <div className="row">
@@ -17,55 +17,54 @@ class Main extends React.Component {
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="row  col-sm-12 col-md-12 col-lg-12 container-fluid">
                 <section className="no-margin padding-top-lg padding-bottom-lg">
-                    <div className="container center">
+                    <div className="container-fluid">     
                         <div className="row">
-                            <div className="col-12 main-text">
-                                <h2 className="text-xxl">Powerful HTTP and GraphQL tool belt</h2>
-                                <p className="text-lg"><strong>Free</strong> and <strong>open source</strong> on Mac, Windows, and Linux</p>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4 padding-top">
-                                <div className="img--promo">
-                                    <div className="gatsby-image-outer-wrapper" >
-                                        <div className="gatsby-image-wrapper" >
-                                            <div></div>
-                                        </div>
+                            <div className="col-sm bg-success">
+                                <div className="col-4 col-sm-4 col-md-4 col-lg-4 padding-top">
+                                    <div className="img--promo">
+                                        <div className="gatsby-image-outer-wrapper" >
+                                            <div className="gatsby-image-wrapper" >
+                                                <div></div>
+                                            </div>
+                                        </div> 
                                     </div>
+                                    <h3>Organize everything</h3>
+                                    <p>Create workspaces or folders, drag-and-drop requests, and easily import and export your data.</p>
                                 </div>
-                                <h3>Create HTTP requests</h3>
-                                <p>Specify URL, payload, headers, and authorization all in one place. Then just hit send.</p>
                             </div>
-
-                            <div className="col-4 padding-top">
-                                <div className="img--promo">
-                                    <div className="gatsby-image-outer-wrapper" >
-                                        <div className="gatsby-image-wrapper" >
-                                            <div></div>
-                                        </div>
+                            <div className="col-sm bg-warning">
+                                <div className="col-4 col-sm-4 col-md-4 col-lg-4 padding-top">
+                                    <div className="img--promo">
+                                        <div className="gatsby-image-outer-wrapper" >
+                                            <div className="gatsby-image-wrapper" >
+                                                <div></div>
+                                            </div>
+                                        </div> 
                                     </div>
+                                    <h3>Organize everything</h3>
+                                    <p>Create workspaces or folders, drag-and-drop requests, and easily import and export your data.</p>
                                 </div>
-                                <h3>View response details</h3>
-                                <p>Get all the details on every response. View status code, body, headers, cookies, and more!</p>
                             </div>
-                                
-                            <div className="col-4 padding-top">
-                                <div className="img--promo">
-                                    <div className="gatsby-image-outer-wrapper" >
-                                        <div className="gatsby-image-wrapper" >
-                                            <div></div>
-                                        </div>
-                                    </div> 
+                            <div className="col-sm bg-success">
+                                <div className="col-4 col-sm-4 col-md-4 col-lg-4 padding-top">
+                                    <div className="img--promo">
+                                        <div className="gatsby-image-outer-wrapper" >
+                                            <div className="gatsby-image-wrapper" >
+                                                <div></div>
+                                            </div>
+                                        </div> 
+                                    </div>
+                                    <h3>Organize everything</h3>
+                                    <p>Create workspaces or folders, drag-and-drop requests, and easily import and export your data.</p>
                                 </div>
-                                <h3>Organize everything</h3>
-                                <p>Create workspaces or folders, drag-and-drop requests, and easily import and export your data.</p>
                             </div>
                         </div>
                     </div>
                 </section>
             </div>
+            
         </div>
     )
   }
