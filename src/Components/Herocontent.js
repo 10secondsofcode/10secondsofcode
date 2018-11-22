f0e40077c88a56c1a216abbe3898811a85177a9f
elangovan <elangovan2me@gmail.com>
2018-11-22 18:57:37 +0530

    Added

--- src/Components/Herocontent.js
+++ src/Components/Herocontent.js
@@ -4,24 +4,13 @@ import React from 'react';
 class Herocontent extends React.Component {
   render() {
     return (
-        <div className="main thememain-white">
+        <div className="main thememain-blue">
             <div className="container main">
-                <div className="row">
-                    <div className="col-sm-12">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                </div>
-                <div className="row"></div>
-                <div className="row">
-                    <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
-                    </div>
-                    <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
-                    </div>
-                    <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
-                    </div>
+                <div className="col-12">
+                    <h2 className="theme-text">The Modern ReactJS Tutorial</h2>
+                    <p className="theme-text" >How it's done now. From the basics to advanced topics with simple, but detailed explanations.
+                        <a className="button button--big" href="#">Download for Linux</a>
+                    </p>
                 </div>
             </div>
         </div>
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,9 +4,9 @@ import React from 'react';
 class Navigation extends React.Component {
   render() {
     return (
-      <div className="main thememain">
+      <div className="main thememain-header">
         <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light thememain">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light thememain-header">
             <a className="navbar-brand" href="#">Navbar</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
