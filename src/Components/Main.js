f80d59fa81794ed592667309544eda45c8f3b044
elangovan <elangovan2me@gmail.com>
2018-11-28 18:18:12 +0530

    Styles Changed..!

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,24 +5,71 @@ class Main extends React.Component {
   render() {
     return (
         <div className="main thememain-white">
-            <div className="container main">
-                <div className="row" >
-                    <div className="col-sm-12">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            <div className="container content-main">
+                <div className="row">
+                    <div className="col-12">
+                        <h3>An introduction : </h3>
                     </div>
                 </div>
-                <div className="row"></div>
                 <div className="row">
                     <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
+                        <ul>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                        </ul>
                     </div>
                     <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
+                        <ul>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>                            
+                        </ul>
                     </div>
                     <div className="col-sm">
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. I
+                        <ul>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                        </ul>
                     </div>
                 </div>
+                
+                <div className="row">
+                    <div className="col-12">
+                        <h3>ReactJs Fundamentals : </h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm">
+                        <ul>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                            <li><a href="#">Introduction Of ReactJs</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm">
+                        <ul>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>
+                            <li><a href="#">Code Editors</a></li>                            
+                        </ul>
+                    </div>
+                    <div className="col-sm">
+                        <ul>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                            <li><a href="#">Developer console</a></li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -7,7 +7,7 @@ class Navigation extends React.Component {
       <div className="main thememain-header">
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light thememain-header">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#" title="10secondsofcode" >10secondsofcode</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,13 +15,13 @@ class Navigation extends React.Component {
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <a className="nav-link" href="#" title="10secondsofcode">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
+                  <a className="nav-link" href="#" title="ReactJs Tutorial">ReactJs</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled" href="#">Disabled</a>
+                  <a className="nav-link disabled" href="#" title="Javascript Tutorial" >Javascript</a>
                 </li>
               </ul>
               <form className="form-inline my-2 my-lg-0">
