e9aa3224cca98413630e55e62aa7058c16f46568
elangovanshanthi <elangovan2me@gmail.com>
2018-12-04 07:28:03 +0530

    add pwa

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import 'jquery';
+import registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById("app"));
+registerServiceWorker();
