424674d6e0f4558e353c15eee55d3051089a4f5e
elangovanshanthi <elangovan2me@gmail.com>
2019-01-21 18:17:26 +0530

    Eslint parse error fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 'off',
     'jsx-a11y/anchor-is-valid': 'off' // temporary solution (while we don't have a router)
-  }
+  },
+  "parser": "babel-eslint"
 }
