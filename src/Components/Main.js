8b067d06a9f47b9be694431f8ec8ca3eff2a33a3
elangovanshanthi <elangovan2me@gmail.com>
2019-03-14 19:56:58 +0530

    added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,75 +1,90 @@
 
 import React from 'react';
+import {title} from './data';
 
-const Main = () => (
-  <div className="main thememain-white">
-    <div className="container content-main">
-      <div className="row">
-        <div className="col-12">
-          <h3>An introduction : </h3>
+class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 5,
+      posts1 : []
+    }    
+  }
+  
+  componentDidMount() {
+    console.log(JSON.stringify(title));
+  }
+
+  render() {
+    return(
+      <div className="main thememain-white">
+        <div className="container content-main">
+          <div className="row">
+            <div className="col-12">
+              <h3>An introduction : </h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+              </ul>
+            </div>
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+              </ul>
+            </div>
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12">
+              <h3>ReactJs Fundamentals : </h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+                <li><a href="#">Introduction Of ReactJs</a></li>
+              </ul>
+            </div>
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+                <li><a href="#">Code Editors</a></li>
+              </ul>
+            </div>
+            <div className="col-sm">
+              <ul>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+                <li><a href="#">Developer console</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-          </ul>
-        </div>
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-          </ul>
-        </div>
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12">
-          <h3>ReactJs Fundamentals : </h3>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-            <li><a href="#">Introduction Of ReactJs</a></li>
-          </ul>
-        </div>
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-            <li><a href="#">Code Editors</a></li>
-          </ul>
-        </div>
-        <div className="col-sm">
-          <ul>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-            <li><a href="#">Developer console</a></li>
-          </ul>
-        </div>
-      </div>
-
-    </div>
-  </div>
-);
-
+    );
+  }
+}
 export default Main;
---
+++ src/Components/data.js
@@ -0,0 +1,26 @@
+export const title = 
+{
+    "Javascript": {
+        "introduction": {
+            "javascript objects" : "javascript objects",
+            "Prototypal inheritance":"Prototypal inheritance",
+            "Scoping and hosting":"Scoping and hosting",
+            "Closures":"Closures",
+            "JavaScript Higher-Order Functions":"JavaScript Higher-Order Functions",
+            "The event loop":"The event loop",
+            "Event bubbling":"Event bubbling",
+            "Apply, call, and bind":"Apply, call, and bind",
+            "Callbacks and promises, async/await":"Callbacks and promises, async/await",
+            "Variable and function hoisting":"Callbacks and promises, async/await",
+            "Currying":"Currying",
+            "localstorage":"Currying",
+            "array fucntions":"Map, filter, reduce , sort of arrays"
+        },
+        "design pattern": {
+            "Decorator" : "Decorator",
+            "Factory":"Factory",
+            "Singleton":"Singleton"
+        }	
+    }
+};
+
